9ed6754b1e699226ef23b395cd9fea86b0d0f9c8
rajukumargiri <rajukumargiri123@gmail.com>
2025-01-28 23:07:56 +0530

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'https://banking-final-api-rajukumargiri123-dev.apps.rm3.7wse.p1.openshiftapps.com/api',
     tokenName :  "authToken",
     origin:'http://localhost:4200' 
   };
